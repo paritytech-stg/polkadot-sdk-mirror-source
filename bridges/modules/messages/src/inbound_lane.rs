paritytech/parity-bridges-common

Introduce MessagesLaneManager (#2227)
* some useful stuff like LanesManager

* clippy

* more clippy

* Error::LanesManager

* {in, out}bound_lane -> active_{in, out}bound_lane

* merge two impl blocks in one

* fmt

modules/messages/src/inbound_lane.rs
@@ -20,8 +20,8 @@ use crate::{BridgedChainOf, Config};
 
 use bp_messages::{
 	target_chain::{DispatchMessage, DispatchMessageData, MessageDispatch},
-	ChainWithMessages, DeliveredMessages, InboundLaneData, LaneId, MessageKey, MessageNonce,
-	OutboundLaneData, ReceivalResult, UnrewardedRelayer,
+	ChainWithMessages, DeliveredMessages, InboundLaneData, LaneId, LaneState, MessageKey,
+	MessageNonce, OutboundLaneData, ReceivalResult, UnrewardedRelayer,
 };
 use bp_runtime::AccountIdOf;
 use codec::{Decode, Encode, EncodeLike, MaxEncodedLen};
@@ -44,6 +44,8 @@ pub trait InboundLaneStorage {
 	fn data(&self) -> InboundLaneData<Self::Relayer>;
 	/// Update lane data in the storage.
 	fn set_data(&mut self, data: InboundLaneData<Self::Relayer>);
+	/// Purge lane data from the storage.
+	fn purge(self);
 }
 
 /// Inbound lane data wrapper that implements `MaxEncodedLen`.
@@ -120,9 +122,21 @@ impl<S: InboundLaneStorage> InboundLane<S> {
 		InboundLane { storage }
 	}
 
-	/// Returns `mut` storage reference.
-	pub fn storage_mut(&mut self) -> &mut S {
-		&mut self.storage
+	/// Get lane state.
+	pub fn state(&self) -> LaneState {
+		self.storage.data().state
+	}
+
+	/// Returns storage reference.
+	pub fn storage(&self) -> &S {
+		&self.storage
+	}
+
+	/// Set lane state.
+	pub fn set_state(&mut self, state: LaneState) {
+		let mut data = self.storage.data();
+		data.state = state;
+		self.storage.set_data(data);
 	}
 
 	/// Receive state of the corresponding outbound lane.
@@ -211,12 +225,17 @@ impl<S: InboundLaneStorage> InboundLane<S> {
 
 		ReceivalResult::Dispatched(dispatch_result)
 	}
+
+	/// Purge lane state from the storage.
+	pub fn purge(self) {
+		self.storage.purge()
+	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use crate::{inbound_lane, tests::mock::*, RuntimeInboundLaneStorage};
+	use crate::{inbound_lane, lanes_manager::RuntimeInboundLaneStorage, tests::mock::*};
 	use bp_messages::UnrewardedRelayersState;
 
 	fn receive_regular_message(

modules/messages/src/outbound_lane.rs
@@ -19,13 +19,13 @@
 use crate::{Config, LOG_TARGET};
 
 use bp_messages::{
-	ChainWithMessages, DeliveredMessages, LaneId, MessageNonce, MessagePayload, OutboundLaneData,
-	UnrewardedRelayer, VerificationError,
+	ChainWithMessages, DeliveredMessages, LaneId, LaneState, MessageNonce, MessagePayload,
+	OutboundLaneData, UnrewardedRelayer, VerificationError,
 };
 use codec::{Decode, Encode};
 use frame_support::{traits::Get, BoundedVec, PalletError, RuntimeDebug};
 use scale_info::TypeInfo;
-use sp_std::{collections::vec_deque::VecDeque, marker::PhantomData};
+use sp_std::{collections::vec_deque::VecDeque, marker::PhantomData, ops::RangeInclusive};
 
 /// Outbound lane storage.
 pub trait OutboundLaneStorage {
@@ -46,6 +46,8 @@ pub trait OutboundLaneStorage {
 	) -> Result<(), VerificationError>;
 	/// Remove outbound message from the storage.
 	fn remove_message(&mut self, nonce: &MessageNonce);
+	/// Purge lane data from the storage.
+	fn purge(self);
 }
 
 /// Limit for the `StoredMessagePayload` vector.
@@ -91,6 +93,24 @@ impl<S: OutboundLaneStorage> OutboundLane<S> {
 	#[cfg(test)]
 	pub fn data(&self) -> OutboundLaneData {
 		self.storage.data()
+	}
+
+	/// Get lane state.
+	pub fn state(&self) -> LaneState {
+		self.storage.data().state
+	}
+
+	/// Set lane state.
+	pub fn set_state(&mut self, state: LaneState) {
+		let mut data = self.storage.data();
+		data.state = state;
+		self.storage.set_data(data);
+	}
+
+	/// Return nonces of all currently queued messages.
+	pub fn queued_messages(&self) -> RangeInclusive<MessageNonce> {
+		let data = self.storage.data();
+		data.oldest_unpruned_nonce..=data.latest_generated_nonce
 	}
 
 	/// Send message over lane.
@@ -155,6 +175,19 @@ impl<S: OutboundLaneStorage> OutboundLane<S> {
 		self.storage.set_data(data);
 
 		Ok(Some(confirmed_messages))
+	}
+
+	/// Remove message from the storage. Doesn't perform any checks.
+	pub fn remove_oldest_unpruned_message(&mut self) {
+		let mut data = self.storage.data();
+		self.storage.remove_message(&data.oldest_unpruned_nonce);
+		data.oldest_unpruned_nonce += 1;
+		self.storage.set_data(data);
+	}
+
+	/// Purge lane state from the storage.
+	pub fn purge(self) {
+		self.storage.purge()
 	}
 }
 

paritytech-stg/polkadot-sdk-mirror-source

bridges/modules/messages/src/inbound_lane.rs
@@ -20,8 +20,8 @@ use crate::{BridgedChainOf, Config};
 
 use bp_messages::{
 	target_chain::{DispatchMessage, DispatchMessageData, MessageDispatch},
-	ChainWithMessages, DeliveredMessages, InboundLaneData, LaneId, MessageKey, MessageNonce,
-	OutboundLaneData, ReceptionResult, UnrewardedRelayer,
+	ChainWithMessages, DeliveredMessages, InboundLaneData, LaneId, LaneState, MessageKey,
+	MessageNonce, OutboundLaneData, ReceptionResult, UnrewardedRelayer,
 };
 use bp_runtime::AccountIdOf;
 use codec::{Decode, Encode, EncodeLike, MaxEncodedLen};
@@ -44,6 +44,8 @@ pub trait InboundLaneStorage {
 	fn data(&self) -> InboundLaneData<Self::Relayer>;
 	/// Update lane data in the storage.
 	fn set_data(&mut self, data: InboundLaneData<Self::Relayer>);
+	/// Purge lane data from the storage.
+	fn purge(self);
 }
 
 /// Inbound lane data wrapper that implements `MaxEncodedLen`.
@@ -120,9 +122,21 @@ impl<S: InboundLaneStorage> InboundLane<S> {
 		InboundLane { storage }
 	}
 
-	/// Returns `mut` storage reference.
-	pub fn storage_mut(&mut self) -> &mut S {
-		&mut self.storage
+	/// Get lane state.
+	pub fn state(&self) -> LaneState {
+		self.storage.data().state
+	}
+
+	/// Returns storage reference.
+	pub fn storage(&self) -> &S {
+		&self.storage
+	}
+
+	/// Set lane state.
+	pub fn set_state(&mut self, state: LaneState) {
+		let mut data = self.storage.data();
+		data.state = state;
+		self.storage.set_data(data);
 	}
 
 	/// Receive state of the corresponding outbound lane.
@@ -211,12 +225,17 @@ impl<S: InboundLaneStorage> InboundLane<S> {
 
 		ReceptionResult::Dispatched(dispatch_result)
 	}
+
+	/// Purge lane state from the storage.
+	pub fn purge(self) {
+		self.storage.purge()
+	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use crate::{inbound_lane, tests::mock::*, RuntimeInboundLaneStorage};
+	use crate::{inbound_lane, lanes_manager::RuntimeInboundLaneStorage, tests::mock::*};
 	use bp_messages::UnrewardedRelayersState;
 
 	fn receive_regular_message(

bridges/modules/messages/src/outbound_lane.rs
@@ -19,16 +19,18 @@
 use crate::{Config, LOG_TARGET};
 
 use bp_messages::{
-	ChainWithMessages, DeliveredMessages, LaneId, MessageNonce, OutboundLaneData, UnrewardedRelayer,
+	ChainWithMessages, DeliveredMessages, LaneId, LaneState, MessageNonce, OutboundLaneData,
+	UnrewardedRelayer,
 };
 use codec::{Decode, Encode};
 use frame_support::{traits::Get, BoundedVec, PalletError};
 use scale_info::TypeInfo;
 use sp_runtime::RuntimeDebug;
-use sp_std::{collections::vec_deque::VecDeque, marker::PhantomData};
+use sp_std::{collections::vec_deque::VecDeque, marker::PhantomData, ops::RangeInclusive};
 
 /// Outbound lane storage.
 pub trait OutboundLaneStorage {
+	/// Stored message payload type.
 	type StoredMessagePayload;
 
 	/// Lane id.
@@ -44,6 +46,8 @@ pub trait OutboundLaneStorage {
 	fn save_message(&mut self, nonce: MessageNonce, message_payload: Self::StoredMessagePayload);
 	/// Remove outbound message from the storage.
 	fn remove_message(&mut self, nonce: &MessageNonce);
+	/// Purge lane data from the storage.
+	fn purge(self);
 }
 
 /// Limit for the `StoredMessagePayload` vector.
@@ -89,6 +93,24 @@ impl<S: OutboundLaneStorage> OutboundLane<S> {
 	/// Get this lane data.
 	pub fn data(&self) -> OutboundLaneData {
 		self.storage.data()
+	}
+
+	/// Get lane state.
+	pub fn state(&self) -> LaneState {
+		self.storage.data().state
+	}
+
+	/// Set lane state.
+	pub fn set_state(&mut self, state: LaneState) {
+		let mut data = self.storage.data();
+		data.state = state;
+		self.storage.set_data(data);
+	}
+
+	/// Return nonces of all currently queued messages.
+	pub fn queued_messages(&self) -> RangeInclusive<MessageNonce> {
+		let data = self.storage.data();
+		data.oldest_unpruned_nonce..=data.latest_generated_nonce
 	}
 
 	/// Send message over lane.
@@ -150,6 +172,19 @@ impl<S: OutboundLaneStorage> OutboundLane<S> {
 		self.storage.set_data(data);
 
 		Ok(Some(confirmed_messages))
+	}
+
+	/// Remove message from the storage. Doesn't perform any checks.
+	pub fn remove_oldest_unpruned_message(&mut self) {
+		let mut data = self.storage.data();
+		self.storage.remove_message(&data.oldest_unpruned_nonce);
+		data.oldest_unpruned_nonce += 1;
+		self.storage.set_data(data);
+	}
+
+	/// Purge lane state from the storage.
+	pub fn purge(self) {
+		self.storage.purge()
 	}
 }
 